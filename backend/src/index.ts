5081465d19077c8ad1bd7d97edd774ebde736c31
dompeteul <dominique.peteul@gmail.com>
2025-07-21 11:58:17 -0400

    Create index.ts

---
+++ backend/src/index.ts
@@ -0,0 +1,8 @@
+
+// backend/src/index.ts
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import path from 'path';
